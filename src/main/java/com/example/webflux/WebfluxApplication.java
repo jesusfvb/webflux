c2ed04976bcfb28e8e0fe21e3e3dbf018faad7b2
Jesús Francisco Vázquez Biltre <jesusfvb2@gmail.com>
2023-03-04 00:48:58 +0100

    Documentacion con Open api

--- src/main/java/com/example/webflux/WebfluxApplication.java
+++ src/main/java/com/example/webflux/WebfluxApplication.java
@@ -1,13 +1,16 @@
 package com.example.webflux;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Webflux API", version = "1.0", description = "Webflux API"))
 public class WebfluxApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebfluxApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxApplication.class, args);
+    }
 
 }
